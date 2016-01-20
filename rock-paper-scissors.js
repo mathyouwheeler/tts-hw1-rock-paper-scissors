c50cef8c00a41345510ff3401b4de705900503a0
Matthew <mw@matthewis.me>
2016-01-20 18:27:44 -0500

    tidy up

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,3 @@
-//- Store the player names and number of wins for each player in variables
 var playerOneName = "Matthew";
 var playerTwoName = "Edwin";
 
@@ -8,67 +7,37 @@ var playerTwoWins = 0;
 var gameIsWon = false;
 var roundCount = 0;
 
-// - Use a while loop to run the game until one player has 3 wins-
 while(!gameIsWon) {
-	// run game
+	roundCount++;
+	//console.log("roundCount= " + roundCount);
+
 	var playerOneHand = parseInt(Math.random()*10)%3;
 	var playerTwoHand = parseInt(Math.random()*10)%3;
-	console.log("playerOneHand= " + playerOneHand);
-	console.log("playerTwoHand= " + playerTwoHand);
+	console.log("Round " + roundCount + " " + playerOneName + " " + playerOneHand);
+	console.log("Round " + roundCount + " " + playerTwoName + " " + playerTwoHand);
 	
-	if (playerOneHand === 0 && playerTwoHand === 0) {
-		console.log("draw");
+	if ((playerOneHand === 0 && playerTwoHand === 0) || (playerOneHand === 1 && playerTwoHand === 1) || (playerOneHand === 2 && playerTwoHand === 2)) {
+		console.log("Round " + roundCount + " is a draw");
 	}
-	if (playerOneHand === 0 && playerTwoHand === 1) {
-		console.log("playerTwoWinsRound =" + playerTwoName);
+
+	if ((playerOneHand === 0 && playerTwoHand === 1) || (playerOneHand === 1 && playerTwoHand === 2) || (playerOneHand === 2 && playerTwoHand === 0)) {
+		console.log("Round " + roundCount + " winner is= " + playerTwoName);
 		playerTwoWins++;
 	}
-	if (playerOneHand === 0 && playerTwoHand === 2) {
-		console.log("playerOneWinsRound =" + playerOneName);
+
+	if ((playerOneHand === 0 && playerTwoHand === 2) || (playerOneHand === 1 && playerTwoHand === 0) || (playerOneHand === 2 && playerTwoHand === 1)) {
+		console.log("Round " + roundCount + " winner is= " + playerOneName);
 		playerOneWins++;
 	}
-	if (playerOneHand === 1 && playerTwoHand === 0) {
-		console.log("playerOneWinsRound =" + playerOneName);
-		playerOneWins++;
-	}
-	if (playerOneHand === 1 && playerTwoHand === 1) {
-		console.log("draw");
-	}
-	if (playerOneHand === 1 && playerTwoHand === 2) {
-		console.log("playerTwoWinsRound =" + playerTwoName);
-		playerTwoWins++;
-	}	
-	if (playerOneHand === 2 && playerTwoHand === 0) {
-		console.log("playerTwoWinsRound =" + playerTwoName);
-		playerTwoWins++;
-	}	
-	if (playerOneHand === 2 && playerTwoHand === 1) {
-		console.log("playerOneWinsRound =" + playerOneName);
-		playerOneWins++;
-	}	
-	if (playerOneHand === 2 && playerTwoHand === 2) {
-		console.log("draw");
-	}	
 
 	if (playerOneWins === 3 || playerTwoWins === 3) {
 		gameIsWon = true;
+
+		if (playerOneWins === 3) {
+			console.log("Game Over! " + playerOneName + " wins the game!");
+		} else {
+			console.log("Game Over! " + playerTwoName + " wins the game!");
+		}	
 	}
 
-	if (playerOneWins === 3) {
-		console.log(playerOneName + " wins!");
-	}
-
-	if (playerTwoWins === 3) {
-		console.log(playerTwoName + " wins!");
-	}
-
-	roundCount++;
-	console.log("roundCount= " + roundCount);
 }
-
-// - Use parseInt(Math.random()*10)%3 to generate a number between 0 and 2 (0 == rock, 1 == paper, 2 == scissors)
-// - Output each players hand to the console
-// - Use if or switch statement to determine a winner of the round
-// - Output the round winner's name to the console.
-// - Keep track of how many rounds each player has won
-// - When one player wins 3 rounds, announce that player's name as the game winner.
